6ba86cd62c7487f38dc3c3fe3c9c5cdc1e840453
Juliana BF <bfjuliana@gmail.com>
2018-03-23 04:21:03 +0000

    Implement forgot and reset password

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,16 @@ var express     = require("express"),
     route       = express.Router(),
     User        = require("../models/user"),
     passport    = require("passport"),
-    middleware  = require("../middleware");
+    middleware  = require("../middleware"),
+    async       = require("async"),
+    nodemailer  = require("nodemailer"),
+    crypto      = require("crypto");
+
+
+//Mailgun Config
+var api_key = 'key-e01c7816a58e48094a1b81cfaf47edd0',
+    domain  = 'sandboxe14a0f41d68d43bfb8e3fb38faaaf9d0.mailgun.org',
+    mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
     
 //ROOT ROUTE   
 route.get("/", function(req,res) {
@@ -16,7 +25,13 @@ route.get('/register', function(req, res) {
 
 //Register authentication
 route.post('/register', function(req, res) {
-    var newUser = new User({username: req.body.username});
+    var newUser = new User({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        avatar: req.body.avatar,
+        email: req.body.email
+    });
     if(req.body.adminCode === "NowYouHaveThePower") {
         newUser.isAdmin = true;
     }
@@ -40,9 +55,11 @@ route.get('/login',function(req, res) {
 //Login Logic
 //app.post('/route', middleware, callback)
 route.post('/login',middleware.checkLogInForm, passport.authenticate("local",{
-    successRedirect: '/campgrounds',
-    failureRedirect: '/login'
-}), function(req, res) {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login",
+        failureFlash: true,
+        successFlash: 'Welcome to YelpCamp!'
+    }), function(req, res) {
    
 });
 
@@ -51,6 +68,165 @@ route.get('/logout', function(req, res) {
    req.logout();
    req.flash("success", "Logged you out!");
    res.redirect('/campgrounds');
+});
+
+//Reset Password
+route.get("/forgot", function(req, res) {
+    res.render("forgot");
+});
+
+route.post("/forgot", function(req,res,next) {
+    async.waterfall([
+        function(done){
+            crypto.randomBytes(20, function(err, buf) {
+                var token = buf.toString('hex');
+                done(err,token);
+            });
+        },
+        function(token,done){
+            User.findOne({email: req.body.email}, function(err, user) {
+                if(err || !user) {
+                    req.flash("error","No account with that email address exists.");
+                    return res.redirect("/forgot");
+                }
+                
+                user.resetPasswordToken = token;
+                user.resetPasswordExpires = Date.now() + 3600000; // Available for 1 hour
+                
+                user.save(function(err) {
+                    done(err,token,user);
+                });
+            });
+        },
+        function(token, user, done) {
+            // var smtpTransport = nodemailer.createTransport({
+            //     service: 'Gmail',
+            //     auth: {
+            //         user: 'bfjuliana@gmail.com',
+            //         pass: process.env.GMAILPW // export GMAILPW=yourpassword
+            //     }
+            // });
+            
+            // smtpTransport.sendMail(mailOptions, function(err) {
+            //     console.log('email sent');
+            //     req.flash("success","An e-mail has been sent to " + user.email + " with further instructions.");
+            //     done(err,'done');
+            // });
+            
+            // var data = {
+            //   from: 'Excited User <jb@madeatmodern.com>',
+            //   to: 'jb@madeatmodern.com',
+            //   subject: 'Hello',
+            //   text: 'Testing some Mailgun awesomeness!'
+            // };
+            
+            var mailOptions = {
+                to: user.email,
+                from: 'no-reply@yelpcamp-julianabf.c9users.io',
+                subject: 'Yelpcamp App - Password Reset',
+                text: 'Hello '+ user.username + ', \n\n' +
+                "We received a request to reset your password for your YelpCamp App account: "+ user.email +
+                ". We're here to help! \n\n Simply click on the link to set a new password: \n\n" +
+                "https://" + req.headers.host + "/reset/" + token + "\n\n" +
+                "If you didn't ask to change your password, don't worry! Your password is still safe and you can delete this email."
+            };
+            mailgun.messages().send(mailOptions, function (error, body) {
+                console.log(body);
+                req.flash("success","An e-mail has been sent to " + user.email + " with further instructions.");
+                done(error,'done');
+            });
+        }
+        ], function(err) {
+            if(err) return next(err);
+            res.redirect("/forgot");
+        });
+});
+
+route.get("/reset/:token", function(req, res) {
+    User.findOne({resetPasswordToken: req.params.token, resetPasswordExpires: {$gt: Date.now()}}, function(err,user) {
+        if(err || !user) {
+            req.flash('error', 'Password reset token is invalid or has expired.');
+            return res.redirect('/forgot');
+        }
+        res.render("reset", {token: req.params.token});
+    });
+});
+
+route.post("/reset/:token", function(req, res) {
+    console.log(req.params.token);
+    async.waterfall([
+        function(done) {
+            User.findOne({resetPasswordToken: req.params.token, resetPasswordExpires: {$gt: Date.now()}}, function(err,user) {
+                if(err || !user) {
+                    req.flash('error', 'Password reset token is invalid or has expired.');
+                    return res.redirect('back');
+                }
+                console.log("user: ", user);
+                if(req.body.password === req.body.confirm) {
+                    user.setPassword(req.body.password, function(err) {
+                        if(err) {
+                            req.flash("Something went wrong!");
+                            return res.redirect("back");
+                        }
+                        user.resetPasswordToken = undefined;
+                        user.resetPasswordExpires = undefined;
+                        
+                        user.save(function(err) {
+                            if(err) {
+                                req.flash("Something went wrong saving the new password.");
+                                return res.redirect("back");
+                            }
+                            req.logIn(user, function(err) {
+                                done(err, user);
+                            });
+                        });
+                    })
+                } else {
+                    req.flash("error", "Passwords do not match.");
+                    res.redirect("back");
+                }
+          });
+      },
+      function(user, done) {
+        //   var smtpTransport = nodemailer.createTransport({
+        //     service: 'Gmail', 
+        //     auth: {
+        //       user: 'bfjuliana@gmail.com',
+        //       pass: process.env.GMAILPW
+        //     }
+        //   });
+        //   var mailOptions = {
+        //     to: user.email,
+        //     from: 'learntocodeinfo@mail.com',
+        //     subject: 'Your password for YelpCamp App has been changed',
+        //     text: 'Hello,'+ user.username +'\n\n' +
+        //       'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+        //   };
+        //   smtpTransport.sendMail(mailOptions, function(err) {
+        //     req.flash('success', 'Success! Your password has been changed.');
+        //     done(err);
+        //   });
+        
+        var mailOptions = {
+                to: user.email,
+                from: 'no-reply@yelpcamp-julianabf.c9users.io',
+                subject: 'Your password for YelpCamp App has been changed',
+                text: 'Hello '+ user.username +', \n\n' +
+                'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+            };
+            mailgun.messages().send(mailOptions, function (error, body) {
+                console.log(body);
+                req.flash('success', 'Success! Your password has been changed.');
+                done(error,'done');
+            });
+        }
+    ], function(err) {
+        if(err) {
+            req.flash("Something went wrong!");
+            return res.redirect("back");
+        }
+        res.redirect('/campgrounds');
+    });
 });
 
 module.exports = route;
