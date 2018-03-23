ef2c5c813d43710a6dbacef31f444f6a263f9134
Juliana BF <bfjuliana@gmail.com>
2018-03-23 04:23:07 +0000

    Add image uploader with multer and cloudinary

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,7 +2,8 @@ var express         = require("express"),
     route           = express.Router(),
     Campground      = require("../models/campground"),
     middleware      = require("../middleware"),
-    NodeGeocoder    = require('node-geocoder');
+    NodeGeocoder    = require('node-geocoder'),
+    multer          = require('multer');
     
 var options         = {
   provider: 'google',
@@ -12,6 +13,29 @@ var options         = {
 };
 
 var geocoder        = NodeGeocoder(options);
+
+//Image uploader config
+
+var storage = multer.diskStorage({
+  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+var imageFilter = function (req, file, cb) {
+    // accept image files only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+var upload = multer({ storage: storage, fileFilter: imageFilter})
+
+var cloudinary = require('cloudinary');
+cloudinary.config({ 
+  cloud_name: 'dnlmdpqac', 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 //INDEX - SHOWS ALL THE CAMPGROUNDS
 route.get("/", function(req,res) {
@@ -26,34 +50,43 @@ route.get("/", function(req,res) {
 });
 
 //CREATE - MAKE A NEW CAMPGROUND AND THEN REDIRECT TO ALL THE LIST 
-route.post("/",middleware.isLoggedIn, function(req,res){ 
+route.post("/",middleware.isLoggedIn, upload.single('image'), function(req,res){ 
     // get data from form and add to campgrounds array
-    var name = req.body.name,
-        image = req.body.image,
-        price = req.body.price,
-        desc = req.body.description,
-        author = {
-            id: req.user._id,
-            username: req.user.username
-        };
-    geocoder.geocode(req.body.location, function (err, data) {
+    // var name = req.body.name,
+    //     image = req.body.image,
+    //     price = req.body.price,
+    //     desc = req.body.description,
+    //     author = {
+    //         id: req.user._id,
+    //         username: req.user.username
+    //     };
+    geocoder.geocode(req.body.campground.location, function (err, data) {
         if (err || !data.length) {
           req.flash('error', 'Invalid address');
           return res.redirect('back');
         }
-        var lat = data[0].latitude;
-        var lng = data[0].longitude;
-        var location = data[0].formattedAddress;
-        var newCamp = {name: name, image: image, description: desc, author:author, price: price, location: location, lat: lat, lng: lng};
+        req.body.campground.lat = data[0].latitude;
+        req.body.campground.lng = data[0].longitude;
+        req.body.campground.location = data[0].formattedAddress;
+        //var newCamp = {name: name, description: desc, author:author, price: price, location: location, lat: lat, lng: lng};
         // Create a new campground and save to DB
-        Campground.create(newCamp,function(err,createdCampground) {
-    		if(err) {
-    			req.flash("err","Campground couldn't be created.");
-    			res.redirect("back");
-    		}else{
-    			 res.redirect("/campgrounds");
-    		}
-    	});
+        cloudinary.uploader.upload(req.file.path, function(result) {
+          // add cloudinary url for the image to the campground object under image property
+          //newCamp.image = result.secure_url;
+          req.body.campground.image = result.secure_url;
+          // add author to campground
+          req.body.campground.author = {
+            id: req.user._id,
+            username: req.user.username
+          }
+          Campground.create(req.body.campground, function(err, campground) {
+            if (err) {
+                req.flash('error', err.message);
+                return res.redirect('back');
+            }
+            res.redirect('/campgrounds/' + campground.id);
+          });
+        });
     });
     
 });
