d491805cffe7c50f5cb61844d7a4b96d39c74e7e
Juliana BF <bfjuliana@gmail.com>
2018-03-29 17:36:53 +0000

    Add change password router

--- routes/users.js
+++ routes/users.js
@@ -41,13 +41,32 @@ route.get("/profile/:id/edit",middleware.checkProfileOwnership, function(req, re
 });
 
 route.put("/profile/:id",middleware.checkProfileOwnership,upload.single('avatar'), function(req, res) { 
-    if(req.file){
-        cloudinary.uploader.upload(req.file.path, function(result) {
-            // add cloudinary url for the image to the campground object under image property
+    if(!req.body.password){
+        if(req.file){
+            cloudinary.uploader.upload(req.file.path, function(result) {
+                // add cloudinary url for the image to the campground object under image property
+                if(req.body.adminCode === "NowYouHaveThePower") {
+                    req.body.user.isAdmin = true;
+                }
+                req.body.user.avatar = result.secure_url;
+                User.findByIdAndUpdate(req.params.id, req.body.user, function(err, updatedUser) {
+                    if(err){
+                        req.flash("error", err.message);
+                        return res.redirect('/users/profile/'+updatedUser._id + "/edit");
+                    } 
+                    if(updatedUser._id.equals(req.params.id)) {
+                        req.flash("success", "Profile updated!");
+                        return res.redirect('/users/profile/'+updatedUser._id + "/edit");
+                    }else {
+                        req.flash("error", "You don't have permission to access this profile.");
+                        return res.redirect('/campgrounds');
+                    }
+                });
+            });
+        }else {
             if(req.body.adminCode === "NowYouHaveThePower") {
                 req.body.user.isAdmin = true;
             }
-            req.body.user.avatar = result.secure_url;
             User.findByIdAndUpdate(req.params.id, req.body.user, function(err, updatedUser) {
                 if(err){
                     req.flash("error", err.message);
@@ -55,46 +74,48 @@ route.put("/profile/:id",middleware.checkProfileOwnership,upload.single('avatar'
                 } 
                 if(updatedUser._id.equals(req.params.id)) {
                     req.flash("success", "Profile updated!");
-                    return res.redirect('/users/profile/'+updatedUser._id + "/edit");
+                    return res.redirect('/users/profile/'+updatedUser._id+ "/edit");
                 }else {
                     req.flash("error", "You don't have permission to access this profile.");
                     return res.redirect('/campgrounds');
                 }
             });
-        });
-    }else {
-        if(req.body.adminCode === "NowYouHaveThePower") {
-            req.body.user.isAdmin = true;
         }
-        User.findByIdAndUpdate(req.params.id, req.body.user, function(err, updatedUser) {
-            if(err){
-                req.flash("error", err.message);
-                return res.redirect('/users/profile/'+updatedUser._id + "/edit");
-            } 
-            if(updatedUser._id.equals(req.params.id)) {
-                req.flash("success", "Profile updated!");
-                return res.redirect('/users/profile/'+updatedUser._id+ "/edit");
-            }else {
-                req.flash("error", "You don't have permission to access this profile.");
-                return res.redirect('/campgrounds');
+    }else {
+        if(req.body.password.length > 1){
+            if(req.body.password === req.body.confirm) {
+                User.findById(req.params.id, function(err, user) {
+                    if(err) {
+                        req.flash("error", err.message);
+                        return res.redirect('/users/profile/'+req.params.id + "/edit");
+                    }
+                    user.setPassword(req.body.password, function(err) {
+                        if(err) {
+                            req.flash("error", err.message);
+                            return res.redirect('/users/profile/'+req.params.id + "/edit");
+                        }
+                        user.save(function(err) {
+                            if(err) {
+                                req.flash("Something went wrong saving the new password.");
+                                return res.redirect("back");
+                            }
+                            req.logIn(user, function(err) {
+                                if(err) {
+                                    req.flash("Something went wrong saving the new password.");
+                                    return res.redirect('/users/profile/'+req.params.id + "/edit");
+                                }
+                                req.flash("Password updated.");
+                                return res.redirect('/campgrounds');
+                            });
+                        });
+                    });
+                });
+            } else {
+                req.flash("error", "New Password and Confirm Password need to be the same.");
+                return res.redirect('/users/profile/'+req.params.id + "/edit");
             }
-        });
+        }
     }
 });
-
-//Implement change password
-// if(req.body.password.length > 1){
-//     if(req.body.password === req.body.confirm) {
-//         updatedUser.setPassword(req.body.password, function(err) {
-//             if(err) {
-//                 req.flash("error", err.message);
-//                 return res.redirect('/users/profile/'+updatedUser._id + "/edit");
-//             }
-//         });
-//     } else {
-//         req.flash("error", "New Password and Confirm Password need to be ");
-//         return res.redirect('/users/profile/'+updatedUser._id + "/edit");
-//     }
-// }
 
 module.exports = route;
