0a5a2e498240ccd9b9f36fc8cd9455e82bfaa93a
julisbf <bfjuliana@gmail.com>
2018-11-16 17:30:01 -0500

    Fix mongoose error

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-require('dotenv').config(); 
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser  = require("body-parser");
-const mongoose  = require("mongoose");
-const flash = require("connect-flash");
-const passport  = require("passport");
-const LocalStrategy = require("passport-local");
-const methodOverride  = require("method-override");
-const User  = require("./models/user.js");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const methodOverride = require('method-override');
+const User = require('./models/user.js');
 
 /* This clean and populate the DB for comments */
-//const seedDB  = require("./seeds"); 
-    
-const commentRoutes = require("./routes/comments");
-const campgroundRoutes  = require("./routes/campgrounds");
-const indexRoutes = require("./routes/index");
-const usersRoutes = require("./routes/users");
+//const seedDB  = require("./seeds");
 
-let urlDataBase = process.env.DATABASEURL || "mongodb://localhost/yelpcamp_app";
-mongoose.connect(urlDataBase);
+const commentRoutes = require('./routes/comments');
+const campgroundRoutes = require('./routes/campgrounds');
+const indexRoutes = require('./routes/index');
+const usersRoutes = require('./routes/users');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine","ejs");
+let urlDataBase = process.env.DATABASEURL || 'mongodb://localhost/yelpcamp_app';
+mongoose.connect(
+  urlDataBase,
+  { useNewUrlParser: true }
+);
+mongoose.set('useCreateIndex', true);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
 app.use(flash());
@@ -30,14 +34,16 @@ app.use(flash());
 app.locals.fns = require('date-fns/distance_in_words_to_now');
 
 /* Seed the database. Creates some data to be display */
-//seedDB(); 
+//seedDB();
 
 //PASSPORT CONFIG
-app.use(require("express-session")({
-  secret: "My mom is my greatest companion!",
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  require('express-session')({
+    secret: 'My mom is my greatest companion!',
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -45,20 +51,20 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req,res,next) {
+app.use(function(req, res, next) {
   res.locals.currentUser = req.user;
-  res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error");
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
   next();
 });
 
 //REQUIRING ROUTES
-app.use("/",indexRoutes);
-app.use("/campgrounds",campgroundRoutes);
-app.use("/campgrounds/:id/comments",commentRoutes);
-app.use("/users",usersRoutes);
+app.use('/', indexRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/comments', commentRoutes);
+app.use('/users', usersRoutes);
 
 //START SERVER
 app.listen(process.env.PORT, process.env.IP, function() {
-  console.log("The YelpCamp server has started!!");
+  console.log('The YelpCamp server has started!!');
 });
